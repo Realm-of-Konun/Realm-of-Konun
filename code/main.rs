6a1f1100583b5d9d09d9e99a701312accef33689
Top-Cat <67204693+blobhead@users.noreply.github.com>
2022-11-08 16:20:54 +0000

    Update main.rs

--- code/main.rs
+++ code/main.rs
@@ -1,18 +1,25 @@
-pub const INDEX;
+use std::prelude;
 
 fn main() {
-  println!("GENERATING MAP");
-  generate_map();
-  println!("CREATING PLAYER");
-  create_map();
+    println!("Generating map");
+    generate_map(1000);
+    println!("Creating player");
+    create_player();
+    println!("Rendering map");
+    render_map();
 }
 
-fn generate_map() {
-  
+fn generate_map(size: i128) {
+    println!("Generating size {} map.", size);
+    if size <= 10 {
+        println!("Map is too small.")
+    } else if size >= 1000000 {
+        println!("Map is too big.");
+ }
 }
 
 fn create_player() {
-
+    
 }
 
 fn render_map() {
