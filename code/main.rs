9764992b00e07c0c3ecc06316682503681b752fc
Top-Cat <67204693+blobhead@users.noreply.github.com>
2022-11-08 07:25:40 +0000

    Update main.rs

--- code/main.rs
+++ code/main.rs
@@ -1,5 +1,20 @@
 pub const INDEX;
 
 fn main() {
+  println!("GENERATING MAP");
+  generate_map();
+  println!("CREATING PLAYER");
+  create_map();
+}
+
+fn generate_map() {
   
+}
+
+fn create_player() {
+
+}
+
+fn render_map() {
+
 }
