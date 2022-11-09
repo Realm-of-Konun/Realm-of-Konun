a0c6be04c1f456cbf72fdf6e69f83a211fd8f2c2
Top-Cat <67204693+blobhead@users.noreply.github.com>
2022-11-09 20:51:58 +0000

    Update main.rs

--- code/main.rs
+++ code/main.rs
@@ -2,69 +2,80 @@
 use std::time;
 use std::thread;
 
-struct Createplayer{
-    health: i32,
-    damage: i32
+struct MainSetup {
+
+}
+
+struct GamePlay {
+
 }
 
 
 fn main() {
-
-    let _player = Createplayer { health: 10 , damage: 10 };
+    let game_over = false;
+    let _mainsetup = MainSetup {};
+    let _gameplay = GamePlay {};
     println!("Setup running");
-    setup();
+    MainSetup::setup();
     println!("Generating map but size check is needed.");
-    generate_map(14, 14) ;
+    MainSetup::generate_map(14, 14) ;
     thread::sleep(time::Duration::from_secs(2));
     println!("Creating player.");
     thread::sleep(time::Duration::from_secs(2));
-    println!("Read player health check: {}. Read player damage check: {}.", _player.health, _player.damage);
-    thread::sleep(time::Duration::from_secs(2));
     println!("Rendering map.");
     thread::sleep(time::Duration::from_secs(2));
-    render_map();
+    MainSetup::render_map();
+    while game_over == false {
+        GamePlay::do_game_tick();
+        GamePlay::do_game_checks();
+        GamePlay::create_enemies();
+    } 
 }
 
-fn generate_map(size_local_x: i128, size_local_y: i128) {
-    println!("Generating size {} by {} map.", size_local_x, size_local_y);
-    if size_local_x & size_local_y <= 10 {
-        println!("Map is too small.")
-    } else if size_local_x & size_local_y >= 100000 {
-        println!("Map is too big.");
-    } else {
-        println!("Size is acceptable. Generating map.")
+impl MainSetup {
+    fn setup() {
+         
+    }
+
+    fn generate_map(size_local_x: i128, size_local_y: i128) {
+        println!("Generating size {} by {} map.", size_local_x, size_local_y);
+        if size_local_x & size_local_y <= 10 {
+            println!("Map is too small.")
+        } else if size_local_x & size_local_y >= 100000 {
+            println!("Map is too big.");
+        } else {
+            println!("Size is acceptable. Generating map.")
+        }
+    }
+
+    fn render_map() {
+        let pause = 500;
+        println!("Beginning pre-render.");
+        thread::sleep(time::Duration::from_millis(pause));
+        println!("Building colour.");
+        thread::sleep(time::Duration::from_millis(pause));
+        println!("Building shaders.");
+        thread::sleep(time::Duration::from_millis(pause));
+        println!("Beginning true render.");
+        thread::sleep(time::Duration::from_millis(pause));
+        println!("Building basic shapes");
+        thread::sleep(time::Duration::from_millis(pause));
+        println!("Final clean up.");
+        thread::sleep(time::Duration::from_millis(pause));
+        println!("System check: complete. Rendering: complete.");
     }
 }
 
+impl GamePlay {
+    fn create_enemies() {
 
+    }
 
+    fn do_game_tick() {
 
+    }
 
-fn render_map() {
-    //let mut percent = 0;
-  
-    //while percent!=101 {
-        //thread::sleep(time::Duration::from_millis(200));
-        //println!("{}% complete rendering.", percent);
-        //percent+=1;
-    //}
+    fn do_game_checks() {
 
-    let pause = 500;
-    println!("Beginning pre-render.");
-    thread::sleep(time::Duration::from_millis(pause));
-    println!("Building colour.");
-    thread::sleep(time::Duration::from_millis(pause));
-    println!("Building shaders.");
-    thread::sleep(time::Duration::from_millis(pause));
-    println!("Beginning true render.");
-    thread::sleep(time::Duration::from_millis(pause));
-    println!("Building basic shapes");
-    thread::sleep(time::Duration::from_millis(pause));
-    println!("Final clean up.");
-    thread::sleep(time::Duration::from_millis(pause));
-    println!("System check: complete. Rendering: complete.");
+    }
 }
-
-fn setup() {
-    
-} 
