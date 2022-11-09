7a5c861fbb2a335ac61d6a00380703351a111e14
Top-Cat <67204693+blobhead@users.noreply.github.com>
2022-11-09 07:56:08 +0000

    Update main.rs

--- code/main.rs
+++ code/main.rs
@@ -1,27 +1,70 @@
-use std::prelude;
+// use std::prelude;
+use std::time;
+use std::thread;
+
+struct Createplayer{
+    health: i32,
+    damage: i32
+}
+
 
 fn main() {
-    println!("Generating map");
-    generate_map(1000);
-    println!("Creating player");
-    create_player();
-    println!("Rendering map");
+
+    let _player = Createplayer { health: 10 , damage: 10 };
+    println!("Setup running");
+    setup();
+    println!("Generating map but size check is needed.");
+    generate_map(14, 14) ;
+    thread::sleep(time::Duration::from_secs(2));
+    println!("Creating player.");
+    thread::sleep(time::Duration::from_secs(2));
+    println!("Read player health check: {}. Read player damage check: {}.", _player.health, _player.damage);
+    thread::sleep(time::Duration::from_secs(2));
+    println!("Rendering map.");
+    thread::sleep(time::Duration::from_secs(2));
     render_map();
 }
 
-fn generate_map(size: i128) {
-    println!("Generating size {} map.", size);
-    if size <= 10 {
+fn generate_map(size_local_x: i128, size_local_y: i128) {
+    println!("Generating size {} by {} map.", size_local_x, size_local_y);
+    if size_local_x & size_local_y <= 10 {
         println!("Map is too small.")
-    } else if size >= 1000000 {
+    } else if size_local_x & size_local_y >= 100000 {
         println!("Map is too big.");
- }
+    } else {
+        println!("Size is acceptable. Generating map.")
+    }
 }
 
-fn create_player() {
-    
-}
+
+
+
 
 fn render_map() {
+    //let mut percent = 0;
+  
+    //while percent!=101 {
+        //thread::sleep(time::Duration::from_millis(200));
+        //println!("{}% complete rendering.", percent);
+        //percent+=1;
+    //}
 
+    let pause = 500;
+    println!("Beginning pre-render.");
+    thread::sleep(time::Duration::from_millis(pause));
+    println!("Building colour.");
+    thread::sleep(time::Duration::from_millis(pause));
+    println!("Building shaders.");
+    thread::sleep(time::Duration::from_millis(pause));
+    println!("Beginning true render.");
+    thread::sleep(time::Duration::from_millis(pause));
+    println!("Building basic shapes");
+    thread::sleep(time::Duration::from_millis(pause));
+    println!("Final clean up.");
+    thread::sleep(time::Duration::from_millis(pause));
+    println!("System check: complete. Rendering: complete.");
 }
+
+fn setup() {
+    
+} 
